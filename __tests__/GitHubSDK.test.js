6af14eafeab602983e6ee65b30194190b140d8ec
Jakub Parol <kubciop33@gmail.com>
2022-05-07 14:22:02 +0200

    Add first test

--- __tests__/GitHubSDK.test.js
+++ __tests__/GitHubSDK.test.js
@@ -1,2 +1,14 @@
 import nodeFetch from 'node-fetch';
 global.fetch = nodeFetch;
+import GitHubSDK from './../src/js/GitHubSDK';
+
+describe('GitHubSDK', () => {
+    it('Should had set name and token when create instance', () => {
+        const name = 'kuba';
+        const token = '123xyz';
+        const newGH = new GitHubSDK(name, token);
+
+        expect(newGH.getName()).toBe(name);
+        expect(newGH.getToken()).toBe(token);
+    })
+})
