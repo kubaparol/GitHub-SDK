a995f996c0f9b3a051bf0e8f3b0811321523a6cf
Jakub Parol <kubciop33@gmail.com>
2022-05-09 11:18:26 +0200

    Tests improvement

--- __tests__/GitHubSDK.test.js
+++ __tests__/GitHubSDK.test.js
@@ -7,12 +7,12 @@ describe('GitHubSDK', () => {
 
     describe('Create instance', () => {
         it('Should create instance when name and token is correct', () => {
-        const name = data.name;
-        const token = data.token;
-        const newGH = new GitHubSDK(name, token);
+            const name = data.name;
+            const token = data.token;
+            const newGH = new GitHubSDK(name, token);
 
-        expect(newGH.name).toBe(name);
-        expect(newGH.token).toBe(token);
+            expect(newGH.name).toBe(name);
+            expect(newGH.token).toBe(token);
         });
     });
 
@@ -21,7 +21,7 @@ describe('GitHubSDK', () => {
             const name = data.name;
             const token = data.token;
             const newGH = new GitHubSDK(name, token);
-    
+
             expect(newGH.getName()).toBe(name);
             expect(newGH.getToken()).toBe(token);
         });
@@ -31,7 +31,7 @@ describe('GitHubSDK', () => {
                 const token = data.token;
                 new GitHubSDK(token);
             }
-    
+
             expect(createWrongGitHubSDK).toThrow('Data are incorrect');
         });
     });
@@ -41,53 +41,71 @@ describe('GitHubSDK', () => {
             const name = data.name;
             const token = data.token;
             const newGH = new GitHubSDK(name, token);
-            const promise = newGH.getUserData();
+            const resp = await newGH.getUserData();
 
-            return await promise.then(resp => expect(typeof resp).toBe('object'));
+            expect(typeof resp).toBe('object');
         });
 
         describe('getUserLogin()', () => {
             it('Should return string with user login when data has been downloaded', async () => {
-            const name = data.name;
-            const token = data.token;
-            const newGH = new GitHubSDK(name, token);
-            const promise = newGH.getUserLogin();
+                const name = data.name;
+                const token = data.token;
+                const newGH = new GitHubSDK(name, token);
+                const resp = await newGH.getUserLogin();
 
-            return await promise.then(resp => expect(typeof resp).toBe('string'));
+                expect(typeof resp).toBe('string');
             });
         });
 
         describe('getUserName()', () => {
             it('Should return string with user name when data has been downloaded', async () => {
-            const name = data.name;
-            const token = data.token;
-            const newGH = new GitHubSDK(name, token);
-            const promise = newGH.getUserName();
+                const name = data.name;
+                const token = data.token;
+                const newGH = new GitHubSDK(name, token);
+                const resp = await newGH.getUserName();
 
-            return await promise.then(resp => expect(typeof resp).toBe('string'));
+                expect(typeof resp).toBe('string');
             });
         });
 
         describe('getUserAvatarUrl()', () => {
             it('Should return string with user avatar url when data has been downloaded', async () => {
-            const name = data.name;
-            const token = data.token;
-            const newGH = new GitHubSDK(name, token);
-            const promise = newGH.getUserAvatarUrl();
-    
-            return await promise.then(resp => expect(typeof resp).toBe('string'));
+                const name = data.name;
+                const token = data.token;
+                const newGH = new GitHubSDK(name, token);
+                const resp = await newGH.getUserAvatarUrl();
+
+                expect(typeof resp).toBe('string');
+            });
+
+            it('Should return string with user avatar url which includes \'avatars.githubusercontent.com\'', async () => {
+                const name = data.name;
+                const token = data.token;
+                const newGH = new GitHubSDK(name, token);
+                const resp = await newGH.getUserAvatarUrl();
+
+                expect(resp.includes('avatars.githubusercontent.com')).toBe(true);
             });
         });
 
         describe('getUserReposUrl()', () => {
             it('Should return string with user repos url when data has been downloaded', async () => {
-            const name = data.name;
-            const token = data.token;
-            const newGH = new GitHubSDK(name, token);
-            const promise = newGH.getUserReposUrl();
+                const name = data.name;
+                const token = data.token;
+                const newGH = new GitHubSDK(name, token);
+                const resp = await newGH.getUserReposUrl();
 
-            return await promise.then(resp => expect(typeof resp).toBe('string'));
+                expect(typeof resp).toBe('string');
             });
+
+            it('Should return string with user repos url which includes \'api.github.com/users/\'', async () => {
+                const name = data.name;
+                const token = data.token;
+                const newGH = new GitHubSDK(name, token);
+                const resp = await newGH.getUserReposUrl();
+
+                expect(resp.includes('api.github.com/users/')).toBe(true);
+            })
         });
     });
 });
